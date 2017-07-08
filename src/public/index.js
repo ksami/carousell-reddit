be405811d0bd46ca21cf408965cf81346a6179a6
ksami <ksami.ihide@gmail.com>
2017-07-09 00:43:00 +0800

    feat(frontend): add click handler to buttons

--- src/public/index.js
+++ src/public/index.js
@@ -1,21 +1,26 @@
 var root = document.body;
 var items = [
-  {author: "user1", votes: 500, text: "test post pls ignore"},
-  {author: "user2", votes: 400, text: "test post pls ignore too"},
-  {author: "user3", votes: 300, text: "test post pls ignore three"},
+  {id: "1", author: "user1", votes: 500, text: "test post pls ignore"},
+  {id: "2", author: "user2", votes: 400, text: "test post pls ignore too"},
+  {id: "3", author: "user3", votes: 300, text: "test post pls ignore three"},
 ];
 
-function clickHandler(action) {
-  console.log("clicked", action);
+function clickHandler(id, action) {
+  // m.request({
+  //   method: "POST",
+  //   url: `/api/v1/topics/${id}/vote`,
+  //   data: {action: action}
+  // }).then(console.log);
+  console.log(id, "clicked", action);
 }
 
 function makeList(items) {
   return m(".list-group",
     items.map(item => m(".list-group-item", m(".row", [
       m(".col-md-1", {style: "display:block; text-align:center"}, [
-        m("a", {onclick: ()=>clickHandler("up")}, m("i.text-center.glyphicon.glyphicon-triangle-top[aria-hidden=true]", {style: "opacity:0.7"})),
+        m("a", {onclick: ()=>clickHandler(item.id, "up")}, m("i.text-center.glyphicon.glyphicon-triangle-top[aria-hidden=true]")),
         m("div", item.votes),
-        m("a", {onclick: ()=>clickHandler("down")}, m("i.text-center.glyphicon.glyphicon-triangle-bottom[aria-hidden=true]", {style: "opacity:0.7"}))
+        m("a", {onclick: ()=>clickHandler(item.id, "down")}, m("i.text-center.glyphicon.glyphicon-triangle-bottom[aria-hidden=true]"))
       ]),
       m(".col-md-11", [
         m("h4.list-group-item-heading", item.text),
